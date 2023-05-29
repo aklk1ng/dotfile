23a6cfbe3d8f16033352f62a88ffe757f66c0aa5
aklk1ng <2628084745@qq.com>
2023-05-29 17:32:06 +0800

    update fish and hypr config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,7 +17,7 @@ source ~/.config/fish/alias.fish
 # run command every login
 if status is-interactive
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx -- -keeptty
+    exec startx
   end
   proxy-clash
 end
