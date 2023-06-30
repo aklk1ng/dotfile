021b1d1b1d07656f082492d03c30c3c9efabf54f
aklk1ng <2628084745@qq.com>
2023-06-30 21:15:54 +0800

    add a `install.sh`

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo pacman -S zsh fish mpc mpc ncmpcpp neofetch ranger rofi tmux
+yay -S joshuto
+stow chrome-flags fish joshuto mpd ncmpcpp neofetch profile ranger rofi st tmux xinit Xresources zim
