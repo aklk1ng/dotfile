a24beebd13a2771ff28caa1d2d90e1fe8aee3dfe
aklk1ng <2628084745@qq.com>
2023-07-01 16:31:11 +0800

    update

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -S zsh fish mpc mpc ncmpcpp neofetch ranger rofi tmux
 yay -S joshuto
-stow chrome-flags fish joshuto mpd ncmpcpp neofetch profile ranger rofi st tmux xinit Xresources zim
+stow chrome-flags fish joshuto mpd ncmpcpp neofetch profile ranger rofi st tmux xinit Xresources 
