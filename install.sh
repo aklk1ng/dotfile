67fbe7b7c06681bb363c64076b9ae7de4fd02aa5
aklk1ng <2628084745@qq.com>
2023-07-03 14:25:01 +0800

    add Nord-Dark theme

--- install.sh
+++ install.sh
@@ -2,4 +2,11 @@
 
 sudo pacman -S zsh fish mpc mpc ncmpcpp neofetch ranger rofi tmux
 yay -S joshuto
+
+echo "linking files..."
 stow chrome-flags fish joshuto mpd ncmpcpp neofetch profile ranger rofi st tmux xinit Xresources 
+
+echo "copy the fcitx5 Nord theme..."
+mkdir -p ~/.local/share/fcitx5/themes/
+cp -r Nord-Dark ~/.local/share/fcitx5/themes/
+echo "ending..."
