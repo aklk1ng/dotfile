f0cb8f7541b7292aa834bf7bb9675fd34101a0a1
aklk1ng <2628084745@qq.com>
2023-07-15 17:31:01 +0800

    del i3 and polybar

--- install.sh
+++ install.sh
@@ -1,21 +1,46 @@
 #! /bin/bash
 
-sudo pacman -S zsh fish mpc mpc ncmpcpp neofetch ranger rofi tmux wofi
-yay -S
+dotfile_dir = "$HOME/dotfile"
 
-yay -S --noconfirm hyprland kitty waybar \
+echo "Install yay"
+sudo pacman -S git base-devel
+cd ~
+sudo git clone https://aur.archlinux.org/yay.git
+cd ./yay
+makepkg -si
+echo "ending..."
+
+echo "Install some packages"
+sudo pacman -S zsh fish mpd mpc ncmpcpp ranger rofi tmux wofi bat ripgrep make cmake fzf
+yay -Syu
+
+yay -S --noconfirm hyprland kitty waybar swww cava \
     swaybg swaylock-effects wofi wlogout mako \
-    ttf-jetbrains-mono-nerd noto-fonts-emoji \
-    polkit-gnome python-requests \
+    noto-fonts-emoji \
+    python-requests \
     swappy grim slurp pamixer brightnessctl gvfs \
     bluez bluez-utils lxappearance xfce4-settings \
     xdg-desktop-portal-hyprland \
-    joshuto waperd
+    joshuto wpaperd lazygit
+echo "ending..."
+
+cd $dotfile_dir
 
 echo "linking files..."
-stow chrome-flags fish joshuto mpd ncmpcpp neofetch profile ranger rofi st tmux xinit Xresources hypr swaylock mako
+stow chrome-flags fish joshuto mpd ncmpcpp profile ranger rofi st tmux xinit Xresources hypr swaylock mako waybar wpaperd dunst fastfetch cava
+echo "ending..."
 
 echo "copy the fcitx5 Nord theme..."
 mkdir -p ~/.local/share/fcitx5/themes/
 cp -r Nord-Dark ~/.local/share/fcitx5/themes/
+echo "ending..."
+
+echo "copy fonts"
+sudo cp $dotfile_dir/JetBrainsMono -r /usr/share/fonts/
+sudo cp $dotfile_dir/NerdFontsSymbolsOnly -r /usr/share/fonts/
+sudo fc-cache -f -v
+echo "ending..."
+
+echo "save fish theme"
+fish_config theme save ayu\ Dark
 echo "ending..."
--- polybar/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-#polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar example &
