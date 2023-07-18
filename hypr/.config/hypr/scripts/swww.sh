2519db5ab0d0b2f12aad0babd54799c5db827d95
aklk1ng <2628084745@qq.com>
2023-07-18 20:30:30 +0800

    update dotfiles

---
+++ hypr/.config/hypr/scripts/swww.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+swww kill
+swww init
+
+export SWWW_TRANSITION_STEP=2
+
+let i=5
+while true; do
+    [ $((i % 500)) -eq 0 ] && swww img $(find ~/wallpaper/bg/ -name "*.png" | shuf -n1)
+    sleep 10
+    let i+=5
+done
--- install.sh
+++ install.sh
@@ -2,32 +2,33 @@
 
 dotfile_dir = "$HOME/dotfile"
 
+cd $dotfile_dir
+sudo pacman -S git unzip stow zsh lxappearance xfce4-settings grim bluez bluez-utils python-requests slurp swappy fish pamixer brightnessctl gvfs mpd mpc ncmpcpp ranger rofi tmux bat ripgrep make cmake fzf fastfetch mdcat lolcat npm yarn pamixer pactl cal yt-dlp dunst network-manager-applet copyq blueman kitty lazygit
 echo "Install yay"
-sudo pacman -S git base-devel
-cd ~
+stow profile
+cd $HOME
 sudo git clone https://aur.archlinux.org/yay.git
 cd ./yay
 makepkg -si
 echo "ending..."
 
 echo "Install some packages"
-sudo pacman -S zsh fish mpd mpc ncmpcpp ranger rofi tmux bat ripgrep make cmake fzf
 yay -Syu
 
-yay -S --noconfirm hyprland kitty waybar swww cava \
-    swaybg swaylock-effects wlogout mako \
-    noto-fonts-emoji \
-    python-requests \
-    swappy grim slurp pamixer brightnessctl gvfs \
-    bluez bluez-utils lxappearance xfce4-settings \
+yay -S --noconfirm hyprland waybar swww cava \
+    swaylock-effects wlogout \
     xdg-desktop-portal-hyprland \
-    joshuto wpaperd lazygit
+    joshuto keyd
+
+echo "Install rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install --git=https://github.com/aklk1ng/hypr-empty
 echo "ending..."
 
 cd $dotfile_dir
 
 echo "linking files..."
-stow chrome-flags fish joshuto mpd ncmpcpp profile ranger rofi st tmux xinit Xresources hypr swaylock mako waybar wpaperd dunst fastfetch cava
+stow chrome-flags fish joshuto mpd ncmpcpp ranger rofi st tmux hypr swaylock waybar dunst fastfetch cava swappy
 echo "ending..."
 
 echo "copy the fcitx5 Nord theme..."
@@ -43,4 +44,22 @@ echo "ending..."
 
 echo "save fish theme"
 fish_config theme save ayu\ Dark
+chsh -s $(which fish)
+echo "ending..."
+
+echo "start mpd service"
+systemctl start mpd.service --user
+echo "ending..."
+
+cd $HOME
+echo "clone repos"
+git clone https://github.com/aklk1ng/yt-dlp.git
+git clone https://github.com/aklk1ng/script.git
+git clone https://github.com/aklk1ng/wallpaper.git
+git clone https://github.com/aklk1ng/keyd.git
+sudo cp keyd/default.conf /etc/keyd/
+sudo keyd --reload
+git clone https://github.com/aklk1ng/st.git $HOME/.config
+cd $HOME/.config/st
+sudo make clean install
 echo "ending..."
