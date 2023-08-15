53abf73bd59c7e71477116d1c45ddee5b76a9276
aklk1ng <2628084745@qq.com>
2023-08-15 17:31:46 +0800

    update waybar config

--- waybar/.config/waybar/scripts/swww.sh
+++ waybar/.config/waybar/scripts/swww.sh
@@ -3,7 +3,7 @@
 swww kill
 swww init
 
-while true; do
-    swww img --transition-duration 1 --transition-fps 90 --transition-type wipe --transition-angle 30 $(find ~/wallpaper/bg/ -name "*.png" | shuf -n1)
+while [[ true ]]; do
+    swww img $(find ~/wallpaper/bg/ -name "*.png" | shuf -n1)
     sleep 300
 done
