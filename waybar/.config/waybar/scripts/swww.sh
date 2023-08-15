71802a99138358f5eb52e710b58182d6ba0a5e20
aklk1ng <2628084745@qq.com>
2023-08-15 17:12:11 +0800

    update bg scripts

--- waybar/.config/waybar/scripts/swww.sh
+++ waybar/.config/waybar/scripts/swww.sh
@@ -3,9 +3,7 @@
 swww kill
 swww init
 
-let i=5
 while true; do
-    [ $((i % 500)) -eq 0 ] && swww img --transition-duration 1 --transition-fps 90 --transition-type wipe --transition-angle 30 $(find ~/wallpaper/bg/ -name "*.png" | shuf -n1)
-    sleep 10
-    let i+=5
+    swww img --transition-duration 1 --transition-fps 90 --transition-type wipe --transition-angle 30 $(find ~/wallpaper/bg/ -name "*.png" | shuf -n1)
+    sleep 300
 done
