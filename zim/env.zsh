4cf72bcb5dfb16eba7a36ae8472b48691fb5560a
aklk1ng <2628084745@qq.com>
2023-10-04 13:08:13 +0800

    del zsh config

--- zim/env.zsh
+++
@@ -1,8 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-export PATH="$HOME/go/bin:$PATH"
-export PATH="$HOME/.erg/bin:$PATH"
-export ERG_PATH="$HOME/.erg"
-export HOMEBREW_NO_AUTO_UPDATE=1
-export EDITOR=/usr/bin/nvim
-eval "$(atuin init zsh)"
--- zim/plugins.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# atuin
-bash <(curl https://raw.githubusercontent.com/ellie/atuin/main/install.sh)
