ed58ba100840a8bd9c09787fa222678f1b70fd6c
aklk1ng <2628084745@qq.com>
2023-10-11 16:14:55 +0800

    update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ dotfile_dir=$HOME/dotfile
 
 cd $dotfile_dir
 sudo pacman -S unzip lxappearance xfce4-settings grim python-requests slurp swappy fish pamixer brightnessctl gvfs mpd mpc ncmpcpp \
-    ranger rofi tmux bat ripgrep cmake fzf mdcat lolcat npm yarn pamixer yt-dlp dunst network-manager-applet copyq kitty lazygit wl-clipboard mupdf eza wofi
+    ranger rofi tmux bat ripgrep cmake fzf mdcat lolcat npm yarn pamixer yt-dlp dunst network-manager-applet copyq kitty lazygit wl-clipboard mupdf eza wofi glow
 bat cache --build
 echo "Install yay"
 stow profile
