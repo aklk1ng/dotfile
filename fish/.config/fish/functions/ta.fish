7d9805a161d901b330f919f225c6117e827d4adb
aklk1ng <2628084745@qq.com>
2023-10-18 18:06:11 +0800

    update fish config and hypr config

---
+++ fish/.config/fish/functions/ta.fish
@@ -0,0 +1,12 @@
+function ta
+    set cnt (count $argv)
+    if test $cnt -ne 2
+        echo "Usage: ta input_file output_extension"
+    else
+        set ex (string match -r '\.([^.]+)$' $argv[1] --group 1)
+        set name (basename -s $ex $argv[1])
+        set new_file $name$argv[2]
+
+        ffmpeg -i $argv[1] $new_file
+    end
+end
