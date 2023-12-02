31dd072c321aeb72e9641c1fb22049517cc0bf90
aklk1ng <2628084745@qq.com>
2023-12-02 16:31:12 +0800

    update fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,22 +1,73 @@
 function fish_prompt --description 'Write out the prompt'
-    # PWD
+    function _git_branch_name
+        set -l branch (git symbolic-ref --quiet HEAD 2>/dev/null)
+        if set -q branch[1]
+            echo (string replace -r '^refs/heads/' '' $branch)
+        else
+            echo (git rev-parse --short HEAD 2>/dev/null)
+        end
+    end
+
+    function _is_git_dirty
+        not command git diff-index --cached --quiet HEAD -- &>/dev/null
+        or not command git diff --no-ext-diff --quiet --exit-code &>/dev/null
+    end
+
+    function _is_git_repo
+        type -q git
+        or return 1
+        git rev-parse --git-dir >/dev/null 2>&1
+    end
+
+    function _is_hg_dirty
+        set -l stat (hg status -mard 2>/dev/null)
+        test -n "$stat"
+    end
+
+    function _is_hg_repo
+        fish_print_hg_root >/dev/null
+    end
+
+    function _repo_branch_name
+        _$argv[1]_branch_name
+    end
+
+    function _is_repo_dirty
+        _is_$argv[1]_dirty
+    end
+
+    function _repo_type
+        if _is_hg_repo
+            echo hg
+            return 0
+        else if _is_git_repo
+            echo git
+            return 0
+        end
+        return 1
+    end
+
+    set -l yellow (set_color -o yellow)
+    set -l magenta (set_color -o magenta)
+    set -l blue (set_color -o blue)
+    set -l green (set_color -o green)
+    set -l normal (set_color -o normal)
+
     set_color C2BD9F
     echo -n (prompt_pwd)
-    set_color normal
 
-    set -q __fish_git_prompt_showdirtystate
-    or set -g __fish_git_prompt_showdirtystate 1
-    set -q __fish_git_prompt_showcolorhints
-    or set -g __fish_git_prompt_showcolorhints 1
-    set -q __fish_git_prompt_color_dirtystate
-    or set -g __fish_git_prompt_color_dirtystate yellow
-    set -q __fish_git_prompt_char_dirtystate
-    or set -g __fish_git_prompt_char_dirtystate '⚡'
-    set -q __fish_git_prompt_color_branch
-    or set -g __fish_git_prompt_color_branch purple
-    fish_vcs_prompt '|%s'
-    echo
+    set -l repo_info
+    if set -l repo_type (_repo_type)
+        set -l repo_branch $magenta(_repo_branch_name $repo_type)
+        set repo_info "$magenta($repo_branch)"
+
+        if _is_repo_dirty $repo_type
+            set -l dirty "$yellow ✗"
+            set repo_info "$repo_info$dirty"
+        end
+    end
+    echo $repo_info
+    set_color normal
 
     echo -n '$ '
-    set_color normal
 end
