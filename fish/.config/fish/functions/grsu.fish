2f43d5236fc672f56e94417fe517352dd2c38e42
aklk1ng <2628084745@qq.com>
2024-01-16 20:34:00 +0800

    update

---
+++ fish/.config/fish/functions/grsu.fish
@@ -0,0 +1,13 @@
+function grsu
+    set cnt $(count $argv)
+    if test $cnt -ne 1 -a $cnt -ne 2
+        echo "Usage: grsu [g] repo_name"
+    else
+        set name (git config --get user.name)
+        if test $argv[1] = "g"
+            git remote set-url origin git@github.com:$name/$argv[2].git
+        else
+            git remote set-url origin https://github.com/$name/$argv[1].git
+        end
+    end
+end
