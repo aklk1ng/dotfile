4d4e37e2ef9925e53e34f24ae3c607fde3f21a05
aklk1ng <2628084745@qq.com>
2024-05-20 16:31:57 +0800

    add cava config

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ ok "ending..."
 
 action "Install packages by paru"
 paru
-paru -S fcitx5-skin-fluentdark-git hyprland \
+paru -S fcitx5-skin-fluentdark-git cava hyprland \
     whitesur-gtk-theme whitesur-icon-theme whitesur-cursor-theme-git \
     swaylock-effects-git rofi-lbonn-wayland-git wlogout \
     xdg-desktop-portal-hyprland \
