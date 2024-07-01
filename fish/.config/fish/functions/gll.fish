5ce6e024e521322a96b81ffdaa1f0324173ca876
aklk1ng <2628084745@qq.com>
2024-07-01 20:30:13 +0800

    update fish

--- fish/.config/fish/functions/gll.fish
+++ fish/.config/fish/functions/gll.fish
@@ -2,7 +2,7 @@ function gll
     if string length -q -- $argv[1]
         git --no-pager log --date=format:'%Y-%m-%d %H:%M' --pretty=format:"%C(magenta)%h %C(yellow)%cd %C(blue)%cn: %C(cyan)%s %C(red)%d%Creset" --graph -n $argv[1]
     else
-        set n 5
+        set n 10
         git --no-pager log --date=format:'%Y-%m-%d %H:%M' --pretty=format:"%C(magenta)%h %C(yellow)%cd %C(blue)%cn: %C(cyan)%s %C(red)%d%Creset" --graph -n $n
     end
 end
