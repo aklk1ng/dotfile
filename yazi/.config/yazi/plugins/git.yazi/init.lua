66bbab9ab77128e408476a77edb0e21e0ea31df0
aklk1ng <2628084745@qq.com>
2024-09-09 21:03:59 +0800

    add git plugin for yazi

--- yazi/.config/yazi/plugins/git.yazi/init.lua
+++ yazi/.config/yazi/plugins/git.yazi/init.lua
@@ -1,242 +1,199 @@
-local function string_split(input, delimiter)
-  local result = {}
+local WIN = ya.target_family() == 'windows'
+local PATS = {
+  { '[MT]', 6 }, -- Modified
+  { '[AC]', 5 }, -- Added
+  { '?$', 4 }, -- Untracked
+  { '!$', 3 }, -- Ignored
+  { 'D', 2 }, -- Deleted
+  { 'U', 1 }, -- Updated
+  { '[AD][AD]', 1 }, -- Updated
+}
 
-  for match in (input .. delimiter):gmatch('(.-)' .. delimiter) do
-    table.insert(result, match)
-  end
-  return result
-end
-
-local function set_status_color(status)
-  if status == nil then
-    return 'green'
-  elseif status == 'M' then
-    return 'red'
-  elseif status == 'A' then
-    return 'red'
-  elseif status == '·' then
-    return 'magenta'
-  elseif status == '?' then
-    return 'yellow'
-  elseif status == 'R' then
-    return 'red'
-  else
-    return 'red'
-  end
-end
-
-local function fix_str_ch(str)
-  local chinese_chars, num_replacements = str:gsub('\\(%d%d%d)', function(s)
-    return string.char(tonumber(s, 8))
-  end)
-  return num_replacements > 0 and chinese_chars:sub(2, -2) or chinese_chars
-end
-
-local function make_git_table(git_status_str)
-  local file_table = {}
-  local git_status
-  local is_dirty = false
-  local filename
-  local multi_path
-  local is_ignore_dir = false
-  local is_untracked_dir = false
-  local convert_name
-  local split_table = string_split(git_status_str:sub(1, -2), '\n')
-  for _, value in ipairs(split_table) do
-    split_value = string_split(value, ' ')
-    if split_value[#split_value - 1] == '' then
-      split_value = string_split(value, '  ')
+local function match(line)
+  local signs = line:sub(1, 2)
+  for _, p in ipairs(PATS) do
+    local path
+    if signs:find(p[1]) then
+      path = line:sub(4, 4) == '"' and line:sub(5, -2) or line:sub(4)
+      path = WIN and path:gsub('/', '\\') or path
     end
-
-    if split_value[#split_value - 1] == '??' then
-      git_status = '?'
-      is_dirty = true
-    elseif split_value[#split_value - 1] == '!!' then
-      git_status = '·'
-    elseif split_value[#split_value - 1] == '->' then
-      git_status = 'R'
-      is_dirty = true
+    if not path then
+    elseif path:find('[/\\]$') then
+      return p[2] == 3 and 30 or p[2], path:sub(1, -2)
     else
-      git_status = split_value[#split_value - 1]
-      is_dirty = true
+      return p[2], path
     end
-    if split_value[#split_value]:sub(-2, -1) == './' and git_status == '·' then
-      is_ignore_dir = true
-      return file_table, is_dirty, is_ignore_dir, is_untracked_dir
-    end
-
-    if split_value[#split_value]:sub(-2, -1) == './' and git_status == '?' then
-      is_untracked_dir = true
-      return file_table, is_dirty, is_ignore_dir, is_untracked_dir
-    end
-
-    multi_path = string_split(split_value[#split_value], '/')
-    if (multi_path[#multi_path] == '' and #multi_path == 2) or git_status ~= '·' then
-      filename = multi_path[1]
-    else
-      filename = split_value[#split_value]
-    end
-
-    convert_name = fix_str_ch(filename)
-    file_table[convert_name] = git_status
   end
-
-  return file_table, is_dirty, is_ignore_dir, is_untracked_dir
 end
 
-local save = ya.sync(
-  function(
-    st,
-    cwd,
-    git_branch,
-    git_file_status,
-    git_is_dirty,
-    git_status_str,
-    is_ignore_dir,
-    is_untracked_dir
-  )
-    if cx.active.current.cwd == Url(cwd) then
-      st.git_branch = git_branch
-      st.git_file_status = git_file_status
-      st.git_is_dirty = git_is_dirty and '*' or ''
-      st.git_status_str = git_status_str
-      st.is_ignore_dir = is_ignore_dir
-      st.is_untracked_dir = is_untracked_dir
-      ya.render()
+local function root(cwd)
+  repeat
+    local cha = fs.cha(cwd:join('.git'))
+    if cha and cha.is_dir then
+      return tostring(cwd)
+    end
+    cwd = cwd:parent()
+  until not cwd
+end
+
+local function bubble_up(changed)
+  local new, empty = {}, Url('')
+  for k, v in pairs(changed) do
+    if v ~= 3 and v ~= 30 then
+      local url = Url(k):parent()
+      while url and url ~= empty do
+        local s = tostring(url)
+        new[s] = (new[s] or 0) > v and new[s] or v
+        url = url:parent()
+      end
     end
   end
-)
+  return new
+end
 
-local clear_state = ya.sync(function(st)
-  st.git_branch = ''
-  st.git_file_status = ''
-  st.git_is_dirty = ''
+local function propagate_down(ignored, cwd, repo)
+  local new, rel = {}, cwd:strip_prefix(repo)
+  for k, v in pairs(ignored) do
+    if v == 30 then
+      if rel:starts_with(k) then
+        new[tostring(repo:join(rel))] = 30
+      elseif cwd == repo:join(k):parent() then
+        new[k] = 3
+      end
+    end
+  end
+  return new
+end
+
+local add = ya.sync(function(st, cwd, repo, changed)
+  st.dirs[cwd] = repo
+  st.repos[repo] = st.repos[repo] or {}
+  for k, v in pairs(changed) do
+    if v == 0 then
+      st.repos[repo][k] = nil
+    elseif v == 30 then
+      st.dirs[k] = ''
+    else
+      st.repos[repo][k] = v
+    end
+  end
   ya.render()
 end)
 
-local function update_git_status(path)
-  ya.manager_emit('plugin', { 'git', args = ya.quote(tostring(path)) })
+local remove = ya.sync(function(st, cwd)
+  local dir = st.dirs[cwd]
+  if not dir then
+    return
+  end
+
+  ya.render()
+  st.dirs[cwd] = nil
+  if not st.repos[dir] then
+    return
+  end
+
+  for _, r in pairs(st.dirs) do
+    if r == dir then
+      return
+    end
+  end
+  st.repos[dir] = nil
+end)
+
+local function setup(st, opts)
+  st.dirs = {}
+  st.repos = {}
+
+  opts = opts or {}
+  opts.order = opts.order or 500
+
+  -- Chosen by ChatGPT fairly, PRs are welcome to adjust them
+  local styles = {
+    [6] = THEME.git_modified and ui.Style(THEME.git_modified) or ui.Style():fg('#ffa500'),
+    [5] = THEME.git_added and ui.Style(THEME.git_added) or ui.Style():fg('#32cd32'),
+    [4] = THEME.git_untracked and ui.Style(THEME.git_untracked) or ui.Style():fg('#a9a9a9'),
+    [3] = THEME.git_ignored and ui.Style(THEME.git_ignored) or ui.Style():fg('#696969'),
+    [2] = THEME.git_deleted and ui.Style(THEME.git_deleted) or ui.Style():fg('#ff4500'),
+    [1] = THEME.git_updated and ui.Style(THEME.git_updated) or ui.Style():fg('#1e90ff'),
+  }
+  -- TODO: Use nerd-font icons as default matching Yazi's default behavior
+  local icons = {
+    [6] = THEME.git_modified and THEME.git_modified.icon or '*',
+    [5] = THEME.git_added and THEME.git_added.icon or '+',
+    [4] = THEME.git_untracked and THEME.git_untracked.icon or '?',
+    [3] = THEME.git_ignored and THEME.git_ignored.icon or '!',
+    [2] = THEME.git_deleted and THEME.git_deleted.icon or '-',
+    [1] = THEME.git_updated and THEME.git_updated.icon or 'U',
+  }
+
+  Linemode:children_add(function(self)
+    local url = self._file.url
+    local dir = st.dirs[tostring(url:parent())]
+    local change
+    if dir then
+      change = dir == '' and 3 or st.repos[dir][tostring(url):sub(#dir + 2)]
+    end
+
+    if not change or icons[change] == '' then
+      return ui.Line('')
+    elseif self._file:is_hovered() then
+      return ui.Line({ ui.Span(' '), ui.Span(icons[change]) })
+    else
+      return ui.Line({ ui.Span(' '), ui.Span(icons[change]):style(styles[change]) })
+    end
+  end, opts.order)
 end
 
-local is_in_git_dir = ya.sync(function(st)
-  return (st.git_branch ~= nil and st.git_branch ~= '') and cx.active.current.cwd or nil
-end)
-
-local flush_empty_folder_status = ya.sync(function(st)
-  local cwd = cx.active.current.cwd
-  local folder = cx.active.current
-  if #folder.window == 0 then
-    clear_state()
-    ya.manager_emit('plugin', { 'git', args = ya.quote(tostring(cwd)) })
+local function fetch(self)
+  local cwd = self.files[1].url:parent()
+  local repo = root(cwd)
+  if not repo then
+    remove(tostring(cwd))
+    return 1
   end
-end)
 
-local handle_path_change = ya.sync(function(st)
-  local cwd = cx.active.current.cwd
-  if st.cwd ~= cwd then
-    st.cwd = cwd
-    clear_state()
-    ya.manager_emit('plugin', { 'git', args = ya.quote(tostring(cwd)) })
+  local paths = {}
+  for _, f in ipairs(self.files) do
+    paths[#paths + 1] = tostring(f.url)
   end
-end)
 
-local M = {
-  setup = function(st, opts)
-    local function linemode_git(self)
-      local f = self._file
-      local git_span = {}
-      local git_status
-      if st.git_branch ~= nil and st.git_branch ~= '' then
-        local name = f.name:gsub('\r', '?', 1)
-        if st.is_ignore_dir then
-          git_status = '·'
-        elseif st.is_untracked_dir then
-          git_status = '?'
-        elseif st.git_file_status and st.git_file_status[name] then
-          git_status = st.git_file_status[name]
-        else
-          git_status = nil
-        end
+  -- stylua: ignore
+  local output, err = Command("git")
+      :cwd(tostring(cwd))
+      :args({ "--no-optional-locks", "-c", "core.quotePath=", "status", "--porcelain", "-unormal", "--no-renames",
+        "--ignored=matching" })
+      :args(paths)
+      :stdout(Command.PIPED)
+      :output()
+  if not output then
+    ya.err('Cannot spawn git command, error code ' .. tostring(err))
+    return 0
+  end
 
-        local color = set_status_color(git_status)
-        if f:is_hovered() then
-          git_span = git_status and ui.Span(git_status .. ' ') or ui.Span('✓ ')
-        else
-          git_span = git_status and ui.Span(git_status .. ' '):fg(color)
-            or ui.Span('✓ '):fg(color)
-        end
-      end
-      return git_span
-    end
-    Linemode:children_add(linemode_git, 8000)
-
-    local function header_git(self)
-      return (st.git_branch and st.git_branch ~= '')
-          and ui.Line({ ui.Span(' <' .. st.git_branch .. st.git_is_dirty .. '>'):fg('green') })
-        or ui.Line({})
-    end
-    Header:children_add(header_git, 1400, Header.LEFT)
-
-    ps.sub('cd', handle_path_change)
-    ps.sub('delete', flush_empty_folder_status)
-    ps.sub('trash', flush_empty_folder_status)
-  end,
-
-  entry = function(_, args)
-    local output
-    local git_is_dirty
-    local is_ignore_dir, is_untracked_dir
-
-    local git_branch
-    local file = io.popen('git symbolic-ref HEAD 2> /dev/null', 'r')
-    if not file then
-      return
-    end
-    output = file:read('*a')
-    file:close()
-
-    if output ~= nil and output ~= '' then
-      local split_output = string_split(output:sub(1, -2), '/')
-
-      git_branch = split_output[3]
-    elseif is_in_git_dir() then
-      git_branch = nil
+  local changed, ignored = {}, {}
+  for line in output.stdout:gmatch('[^\r\n]+') do
+    local sign, path = match(line)
+    if sign == 30 then
+      ignored[path] = sign
     else
-      return
+      changed[path] = sign
     end
-
-    local git_status_str = ''
-    local git_file_status = nil
-    local file = io.popen('git status --ignored -s --ignore-submodules=dirty 2> /dev/null', 'r')
-    if not file then
-      return
-    end
-    output = file:read('*a')
-    file:close()
-
-    if output ~= nil and output ~= '' then
-      git_status_str = output
-      git_file_status, git_is_dirty, is_ignore_dir, is_untracked_dir =
-        make_git_table(git_status_str)
-    end
-    save(
-      args[1],
-      git_branch,
-      git_file_status,
-      git_is_dirty,
-      git_status_str,
-      is_ignore_dir,
-      is_untracked_dir
-    )
-  end,
-}
-
-function M:fetch()
-  local path = is_in_git_dir()
-  if path then
-    update_git_status(path)
   end
+
+  if self.files[1].cha.is_dir then
+    ya.dict_merge(changed, bubble_up(changed))
+    ya.dict_merge(changed, propagate_down(ignored, cwd, Url(repo)))
+  else
+    ya.dict_merge(changed, propagate_down(ignored, cwd, Url(repo)))
+  end
+
+  for _, p in ipairs(paths) do
+    local s = p:sub(#repo + 2)
+    changed[s] = changed[s] or 0
+  end
+  add(tostring(cwd), repo, changed)
+
   return 3
 end
 
-return M
+return { setup = setup, fetch = fetch }
