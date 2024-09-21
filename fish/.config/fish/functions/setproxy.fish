560f3935900137e01daa238b5ca002f1907fc137
aklk1ng <2628084745@qq.com>
2024-09-21 21:38:28 +0800

    update

--- fish/.config/fish/functions/setproxy.fish
+++ fish/.config/fish/functions/setproxy.fish
@@ -1,3 +1,3 @@
 function setproxy
-    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 ALL_PROXY=socks5:://127.0.0.1:7890
+    export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 ALL_PROXY=socks5:://127.0.0.1:7891
 end
