26811210102a3ea0958a3e7763fe62d9a430519c
aklk1ng <2628084745@qq.com>
2024-09-27 16:19:52 +0800

    update fish

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,8 +3,12 @@ function fish_user_key_bindings
 
     bind -M insert \ca beginning-of-line
     bind -M insert \ce end-of-line
+    bind -M insert \cn history-search-forward
+    bind -M insert \cp history-search-backward
+
     for mode in insert default visual
-        bind -M $mode \cf accept-autosuggestion
+        bind -M $mode \cf forward-char
+        bind -M $mode \cb backward-char
     end
 
     for mode in insert default
