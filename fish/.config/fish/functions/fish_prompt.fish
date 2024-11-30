b14e31bc0973c6aee58d2d8b6d7e89774234bab0
aklk1ng <2628084745@qq.com>
2024-11-30 19:47:35 +0800

    update fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,17 @@
 function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+    set -q fish_color_status
+    or set -g fish_color_status red
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
     echo -n (prompt_pwd)
 
     set -q __fish_git_prompt_showdirtystate
@@ -30,6 +43,7 @@ function fish_prompt --description 'Write out the prompt'
     set -q __fish_git_prompt_char_stateseparator
     or set -g __fish_git_prompt_char_stateseparator ''
     fish_vcs_prompt '|%s'
+    echo -n " "$prompt_status
     echo
 
     set_color cyan
