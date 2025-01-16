a0d2204fe0d2c270e5a83c116c4d3133a0c25234
aklk1ng <2628084745@qq.com>
2025-01-16 20:35:08 +0800

    Revert "update fish"
    This reverts commit 7316b2592dd9c5d7784809bfd362b438a6bd60ac.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,7 @@ source $HOME/.config/fish/env.fish
 
 setproxy
 set fish_vi_force_cursor 1
-set fish_cursor_default line
+set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
